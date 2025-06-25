b953ab8beea287711b10e51e31dc59936fea10ef
slo-han <hanyumad12@naver.com>
2025-06-25 16:45:05 +0900

    Create ArticleRepository.java

---
+++ ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.example.Kopoproject.repository;
+
+import com.example.Kopoproject.entity.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+}
