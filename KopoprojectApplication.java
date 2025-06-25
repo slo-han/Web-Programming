adee6c4040d96335040c68e964035f0a0a734a78
slo-han <hanyumad12@naver.com>
2025-06-25 16:45:57 +0900

    Create KopoprojectApplication.java

---
+++ KopoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kopoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopoprojectApplication.class, args);
+	}
+
+}
